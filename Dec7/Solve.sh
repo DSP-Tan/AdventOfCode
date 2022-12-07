78a00998410b64ba0007d2edf2f718d42ff5459c
Daniel Tanner <danielsptanner@gmail.com>
2022-12-08 00:30:45 +0100

    Unnecessary quotation marks removed

--- Dec7/Solve.sh
+++ Dec7/Solve.sh
@@ -17,13 +17,13 @@ chmod +x Salve.sh && ./Salve.sh
 # Part 1
 directs=$(ls -R | grep : | tr ":" " ")
 for i in $directs ; do
-   ls -R $i | grep "[0-9]" | awk '{s+=$1} END {print s}'
-done                       | awk '{if($1<= 100000) s+=$1} END {print s}'
+   ls -R $i | grep [0-9] | awk '{s+=$1} END {print s}'
+done                     | awk '{if($1<= 100000) s+=$1} END {print s}'
 
 # Part 2
 Total=70000000; Needs=30000000;
-UsedS=$(ls -R . | grep "[0-9]" | awk '{s+=$1} END {print s}')
+UsedS=$(ls -R . | grep [0-9] | awk '{s+=$1} END {print s}')
 Smlst=$( echo "$Needs - ($Total - $UsedS)" | bc )
 
-for i in $directs ; do ls -R $i | grep "[0-9]" | awk '{s+=$1} END {print s}'
+for i in $directs ; do ls -R $i | grep [0-9] | awk '{s+=$1} END {print s}'
 done | awk '{if($1 >= '$Smlst') print $1}' | sort -n | head -n1
