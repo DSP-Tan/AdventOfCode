766a3f2330621750b1ae5006322e0b2cc7530581
Daniel Tanner <danielsptanner@gmail.com>
2022-12-09 01:40:11 +0100

    Good part1

---
+++ Dec8/Part1_good.py
@@ -0,0 +1,18 @@
+import numpy as np
+Lines=open("input.txt","r").read().split()
+matrix = np.array( [ [int(i) for i in j ] for j in [ list(i) for i in Lines ]] )
+N=matrix.shape[0]
+
+
+def viz(i,j):
+    up    = all(matrix[0:i,j]   < matrix[i,j])
+    down  = all(matrix[i+1:N,j] < matrix[i,j]) 
+    left  = all(matrix[i,0:j]   < matrix[i,j])
+    right = all(matrix[i,j+1:N] < matrix[i,j])
+    return up or down or left or right 
+
+count= 4*N-4 # Edge trees are all visible
+for i in range(1,N-1):
+    for j in range(1,N-1):
+        count+= viz(i,j)
+print(count)
