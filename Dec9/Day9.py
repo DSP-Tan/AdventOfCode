5cde61de95d0f93fa74102309b85bd543750e9dd
Daniel Tanner <danielsptanner@gmail.com>
2022-12-09 20:27:10 +0100

    First short version Day 9

--- Dec9/Day9.py
+++ Dec9/Day9.py
@@ -10,29 +10,25 @@ def react(head,tail):
        return tail + np.array([dx, dy ])
     return tail
 
-
-grid = np.zeros((1000,1000),dtype=int)
+grid1 = np.zeros((1000,1000),dtype=int)
 head=np.array([0,0])
 tail=np.array([0,0])
-grid[tuple(tail)] = 1
+grid1[tuple(tail)] = 1
+
+grid2 = np.zeros((1000,1000),dtype=int)
+rope  = [np.array([0,0])]*10
+grid2[tuple(rope[9])] = 1
+
 mov_dict={"R":(0,1), "L":(0,-1), "U":(-1,0), "D":(1,0)}
 for move in moves:
     for i in range(move[1]):
         head += mov_dict[move[0]]
         tail = react(head,tail).astype(int)
-        grid[tuple(tail)] = 1
-print(grid.sum())
+        grid1[tuple(tail)] = 1
 
-
-
-grid = np.zeros((1000,1000),dtype=int)
-rope= [np.array([0,0])]*10
-grid[tuple(rope[9])] = 1
-mov_dict={"R":(0,1), "L":(0,-1), "U":(-1,0), "D":(1,0)}
-for move in moves:
-    for i in range(move[1]):
         rope[0] += mov_dict[move[0]]
         for i, knot in enumerate(rope[1:]):
             rope[i+1] = react(rope[i],rope[i+1]).astype(int)
-        grid[tuple(rope[9])] = 1
-print(grid.sum())
+        grid2[tuple(rope[9])] = 1
+print(grid1.sum())
+print(grid2.sum())
