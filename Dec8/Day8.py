97f92a4ff36101925e60c0304f852f991070b911
Daniel Tanner <danielsptanner@gmail.com>
2022-12-09 12:34:35 +0100

    Removed single use functions

--- Dec8/Day8.py
+++ Dec8/Day8.py
@@ -3,18 +3,9 @@ Lines=open("input.txt","r").read().split()
 matrix = np.array( [ [int(i) for i in j ] for j in [ list(i) for i in Lines ]] )
 N=matrix.shape[0]
 
-def trees(i,j):
-    up    = np.flip(matrix[0:i,j] < matrix[i,j])
-    down  = matrix[i+1:N,j]       < matrix[i,j]
-    left  = np.flip(matrix[i,0:j] < matrix[i,j])
-    right = matrix[i,j+1:N]       < matrix[i,j]
-    return (up,down,left,right)
-
-def scenic(i,j, direction):
-    up, down, left, right = trees(i,j)
-    dir={"up":up,"down":down,"left":left,"right":right}
+def scenic(direction):
     count=0
-    for tree in dir[direction]:
+    for tree in direction:
         if tree:
             count +=1
         else:
@@ -23,12 +14,16 @@ def scenic(i,j, direction):
     return count
 
 scenes=np.zeros((N,N))
-count= 4*N-4 # Edge trees are all visible
+visible = 4*N-4 # Edge trees are all visible
 for i in range(1,N-1):
     for j in range(1,N-1):
-        up, down, left, right = trees(i,j)
-        count += all(up) or all(down) or all(left) or all(right)
-        counts = [ scenic(i,j,direc) for direc in ["up","down","left","right"] ]
+        up    = np.flip(matrix[0:i,j] < matrix[i,j])
+        down  = matrix[i+1:N,j]       < matrix[i,j]
+        left  = np.flip(matrix[i,0:j] < matrix[i,j])
+        right = matrix[i,j+1:N]       < matrix[i,j]
+
+        visible += all(up) or all(down) or all(left) or all(right)
+        counts = [ scenic(dir) for dir in [up,down,left,right] ]
         scenes[i,j] = counts[0]*counts[1]*counts[2]*counts[3]
-print(f"Part 1: {count}")
+print(f"Part 1: {visible}")
 print(f"Part 2: {scenes.max()}")
