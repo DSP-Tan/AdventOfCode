f7faf6fafde65fec4d0944431752406fbbfeb26e
Daniel Tanner <danielsptanner@gmail.com>
2022-12-11 00:33:40 +0100

    normal

--- Dec10/Day10.py
+++ Dec10/Day10.py
@@ -5,23 +5,24 @@ cycles=[(cycle,X)]
 for i in Lines:
     if i[0]=="noop":
         cycle +=1
-        cycles.append((cycle,X))
     else:
         for _ in range(1):
             cycle +=1
             cycles.append((cycle,X))
         cycle+=1
         X +=int(i[1])
-        cycles.append((cycle,X))
+    cycles.append((cycle,X))
 
-sum=0
+# Part 1
+tot=0
 for i in cycles:
     if (i[0]-20)%40 == 0:
-        sum+= i[0]*i[1]
-print(sum)
+        tot+= i[0]*i[1]
+print(tot)
 
+# Part 2
 for i in cycles:
-    if i[1]-1<=(i[0]-1)%40<=i[1]+1:
+    if i[1]-1<= (i[0]-1)%40 <= i[1]+1:
         print("#",end="")
     else:
         print('.',end="")
