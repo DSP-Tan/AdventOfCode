671e838d8bb7c57ef6fcdeeccb234da92d5b34a2
Daniel Tanner <danielsptanner@gmail.com>
2022-12-21 17:36:00 +0100

    Refactored and removed deprecated methods. Compiled and ran on linux and not just through scala-spark shell

--- Dec1/Day1.scala
+++ Dec1/Day1.scala
@@ -1,34 +1,32 @@
 import scala.io.Source
 
 def max_below(input:String, below: Int): Int ={
-   val Lines = Source.fromFile(input).getLines.toList
-   var cals  :Int = 0
-   var max   :Int = 0
-   for(line<-Lines)
-      if( !line.isBlank() ){
-        cals = cals + line.toInt
-      }else{ 
-        if(max<cals && cals < below)
-           max=cals
-        cals =0
-        }
+    val Lines = Source.fromFile(input).getLines().toList
+    var cals  :Int = 0
+    var max   :Int = 0
+    for(line<-Lines)
+       if( !line.isBlank() ){
+         cals +=  line.toInt
+       }else{
+         if(max<cals && cals < below)
+            max=cals
+         cals =0
+         }
     return max
     }
 
-// Part 1
-var init: Int=99999
-var sum: Int=0
-for(i<-Range(0,1)){
-    init= max_below("input.txt",init)
-    sum +=init
+def sum_maxes(input: String, maxes: Int): Int ={
+    var init1: Int=99999
+    var sum:  Int=0
+    for(i<-Range(0,maxes)){
+        init1 = max_below(input,init1)
+        sum +=init1
+        }
+    return sum
     }
-println(sum)
+
+// Part 1
+println(sum_maxes("input.txt", 1))
 
 //Part 2
-var init: Int=99999
-var sum: Int=0
-for(i<-Range(0,3)){
-    init= max_below("input.txt",init)
-    sum +=init
-    }
-println(sum)
+println(sum_maxes("input.txt",3))
