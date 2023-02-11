35d5be577918f2624e7de0d414978602ffcf74a9
Daniel Tanner <danielsptanner@gmail.com>
2023-02-11 18:29:59 +0100

    script to compile and run official solution

---
+++ Dec1/compile.sh
@@ -0,0 +1,10 @@
+# To compile and run official solution:
+
+# Note modifications to code, ecapsulating it in a standalone object:
+# Compile like this:
+
+scalac Day1_soln.scala
+# Run like this:
+fart=$(cat input.txt)
+
+scala Official $fart
