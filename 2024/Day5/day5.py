98be27719c0f72f954a3c5532187877f9b2afa4e
DSP-Tan <danielsptanner@gmail.com>
2024-12-05 23:13:15 +0100

    to do added

--- 2024/Day5/day5.py
+++ 2024/Day5/day5.py
@@ -1,3 +1,10 @@
+
+# The repition that is being removed by doing "set(bads)" should probably
+# j ust be avoided here. This algorithm should be better made
+
+# This problem would be cool to solve in C by just passing the rules as the comparison
+# function to quickSort.
+
 from math import ceil
 pageLists = [[int(j) for j in i.strip("\n").split(",")] for i in open("input_pages.txt","r").readlines()]
 rules     = [[int(j) for j in i.strip("\n").split("|")] for i in open("input_orders.txt").readlines() ]
